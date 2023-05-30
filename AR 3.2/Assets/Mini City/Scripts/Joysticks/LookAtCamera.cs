3724b12a04c93b5ef9740055d8f71d32caffd6d0
shanti <shanti.rize@gmail.com>
2023-05-30 17:56:06 +0300

    up- -map scene

---
+++ AR 3.2/Assets/Mini City/Scripts/Joysticks/LookAtCamera.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class LookAtCamera : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Camera.main != null)
+        {
+            transform.LookAt(Camera.main.transform);
+        }
+    }
+}
